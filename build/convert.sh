552992467afc3739b40e27801926a66ebc34e064
Nick Thorp <nickthorp20@gmail.com>
2018-12-19 19:44:12 -0600

    Fixing convert script

--- build/convert.sh
+++ build/convert.sh
@@ -6,5 +6,4 @@ docker rm sass-converter
 # Build sass-converter if necessary
 docker build --rm -t sass:latest -f `pwd`/thebideo/docker/sass/Dockerfile .
 # Run that shit!
-docker run  --rm sass:latest
 docker run -d -v `pwd`/thebideo/_sass/:/src -v `pwd`/thebideo/bideosite/static/bideosite/:/dest --name sass-converter -it sass:latest
