b78ac461bfe408ac14f23c6b29fea533e6b56406
Nick Thorp <nickthorp20@gmail.com>
2018-12-22 01:34:14 -0600

    Debugging static files

--- build/thebideo-docker.sh
+++ build/thebideo-docker.sh
@@ -2,17 +2,8 @@
 
 # $BUILD_NUMBER is an environment variable intended to be in inherited from build automation (Jenkins)
 
-# Stop and Remove and existing thebideo containers
-docker stop thebideo
-docker rm thebideo
-
 # Remove any existing images
 docker image rm thebideo:latest
 
 # Build that container
 docker build --rm -t thebideo:latest -t thebideo:1.${BUILD_NUMBER} -f `pwd`/thebideo/docker/thebideo/Dockerfile .
-
-# Run the container, should not be part of the build steps
-# docker run -d -p 127.0.0.1:19000:9000 -p 127.0.0.1:19191:9191 --name thebideo -it thebideo:latest
-# Run the DB migrate command, also should not be run during build time
-# docker exec -it django python manage.py migrate
