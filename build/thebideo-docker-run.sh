539f237e5d32c5ee68c797e9638ada9b0d95ae79
Nick Thorp <nickthorp20@gmail.com>
2018-12-28 21:06:29 -0600

    Fixing the docker commands in the deploy file

--- build/thebideo-docker-run.sh
+++ build/thebideo-docker-run.sh
@@ -6,11 +6,11 @@ VOLUMES="-v /etc/thebideo:/etc/thebideo"
 DOCKER_ENV="--env ENV=PRODUCTION"
 
 # Stop and Remove and existing thebideo containers
-docker stop nuggle/thebideo-unchained
-docker rm nuggle/thebideo-unchained
+docker stop thebideo-unchained
+docker rm thebideo-unchained
 
 # Run the container, should not be part of the build steps
-docker run -d ${PORTS} ${VOLUMES} ${DOCKER_ENV} --name nuggle/thebideo-unchained -it nuggle/thebideo-unchained:1.$BUILD_NUMBER
+docker run -d ${PORTS} ${VOLUMES} ${DOCKER_ENV} --name thebideo-unchained -it nuggle/thebideo-unchained:1.$BUILD_NUMBER
 # Run the DB migrate command, also should not be run during build time
 sleep 10 # gotta give it some time to warm up
-docker exec -i nuggle/thebideo-unchained python manage.py migrate
+docker exec -i thebideo-unchained python manage.py migrate
